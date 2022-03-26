342e96e1dfe27ce56bd148d355d74bafe1d60ef2
Reuben Gardos Reid <ReubenJ@users.noreply.github.com>
2022-03-26 17:20:52 +0100

    add vedatad MultiThumos dataset loader

--- vedatad/datasets/__init__.py
+++ vedatad/datasets/__init__.py
@@ -4,9 +4,10 @@ from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
                                RepeatDataset)
 from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
 from .thumos14 import Thumos14Dataset
+from .multithumos import MultiThumosDataset
 
 __all__ = [
-    'CustomDataset', 'Thumos14Dataset', 'GroupSampler',
+    'CustomDataset', 'Thumos14Dataset', 'MultiThumosDataset', 'GroupSampler',
     'DistributedGroupSampler', 'DistributedSampler', 'build_dataloader',
     'ConcatDataset', 'RepeatDataset', 'ClassBalancedDataset', 'build_dataset'
 ]
